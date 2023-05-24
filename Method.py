4e9cbbfc7b4cca31b2a459a04e4c6ba084344968
Henok100 <henokgashaw15@gmail.com>
2023-05-24 16:01:12 +0300

    Final

--- Method.py
+++ Method.py
@@ -8,17 +8,17 @@ import socket
 df = []
 XYZ_NumPyArray = []
 
-# csv_filename = ['_20m/_20m_0_path_test.csv', \
-#                 '_20m/_20m_1_path_test.csv', \
-#                 '_20m/_20m_2_path_test.csv', \
-#                 '_20m/_20m_3_path_test.csv', \
-#                 '_20m/_20m_4_path_test.csv', \
-#                 '_20m/_20m_5_path_test.csv', \
-#                 '_20m/_20m_6_path_test.csv', \
-#                 '_20m/_20m_7_path_test.csv', \
-#                 '_20m/_20m_8_path_test.csv', \
-#                 '_20m/_20m_9_path_test.csv',
-#                 '_20m/_20m_10_path_test.csv']
+csv_filename = ['_20m/_20m_0_path_test.csv', \
+                '_20m/_20m_1_path_test.csv', \
+                '_20m/_20m_2_path_test.csv', \
+                '_20m/_20m_3_path_test.csv', \
+                '_20m/_20m_4_path_test.csv', \
+                '_20m/_20m_5_path_test.csv', \
+                '_20m/_20m_6_path_test.csv', \
+                '_20m/_20m_7_path_test.csv', \
+                '_20m/_20m_8_path_test.csv', \
+                '_20m/_20m_9_path_test.csv', \
+                '_20m/_20m_10_path_test.csv']
 
 # csv_filename = ['_60m/_60m_0_path_test.csv', \
 #                 '_60m/_60m_1_path_test.csv', \
@@ -29,19 +29,20 @@ XYZ_NumPyArray = []
 #                 '_60m/_60m_6_path_test.csv', \
 #                 '_60m/_60m_7_path_test.csv', \
 #                 '_60m/_60m_8_path_test.csv', \
-#                 '_60m/_60m_9_path_test.csv']
+#                 '_60m/_60m_9_path_test.csv', \
+#                 '_60m/_60m_10_path_test.csv']
 
-csv_filename = ['_120m/_120m_0_path_test.csv', \
-                '_120m/_120m_1_path_test.csv', \
-                '_120m/_120m_2_path_test.csv', \
-                '_120m/_120m_3_path_test.csv', \
-                '_120m/_120m_4_path_test.csv', \
-                '_120m/_120m_5_path_test.csv', \
-                '_120m/_120m_6_path_test.csv', \
-                '_120m/_120m_7_path_test.csv', \
-                '_120m/_120m_8_path_test.csv', \
-                '_120m/_120m_9_path_test.csv',
-                '_120m/_120m_10_path_test.csv']
+# csv_filename = ['_120m/_120m_0_path_test.csv', \
+#                 '_120m/_120m_1_path_test.csv', \
+#                 '_120m/_120m_2_path_test.csv', \
+#                 '_120m/_120m_3_path_test.csv', \
+#                 '_120m/_120m_4_path_test.csv', \
+#                 '_120m/_120m_5_path_test.csv', \
+#                 '_120m/_120m_6_path_test.csv', \
+#                 '_120m/_120m_7_path_test.csv', \
+#                 '_120m/_120m_8_path_test.csv', \
+#                 '_120m/_120m_9_path_test.csv', \
+#                 '_120m/_120m_10_path_test.csv']
 
 def PortList(numUavs):
     PORT = []
