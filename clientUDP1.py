3b4107c6486079c110d1053d37fc70f8c0dca5f0
Henok100 <henokgashaw15@gmail.com>
2023-05-30 17:32:08 +0300

    Final

--- clientUDP1.py
+++ clientUDP1.py
@@ -35,29 +35,29 @@ ADDR_Protocol = (CLIENT, PORT_Protocol)
 #                 '_20m/_20m_9_path_test.csv', \
 #                 '_20m/_20m_10_path_test.csv']
 
-csv_filename = ['_60m/_60m_0_path_test.csv', \
-                '_60m/_60m_1_path_test.csv', \
-                '_60m/_60m_2_path_test.csv', \
-                '_60m/_60m_3_path_test.csv', \
-                '_60m/_60m_4_path_test.csv', \
-                '_60m/_60m_5_path_test.csv', \
-                '_60m/_60m_6_path_test.csv', \
-                '_60m/_60m_7_path_test.csv', \
-                '_60m/_60m_8_path_test.csv', \
-                '_60m/_60m_9_path_test.csv', \
-                '_60m/_60m_10_path_test.csv']
+# csv_filename = ['_60m/_60m_0_path_test.csv', \
+#                 '_60m/_60m_1_path_test.csv', \
+#                 '_60m/_60m_2_path_test.csv', \
+#                 '_60m/_60m_3_path_test.csv', \
+#                 '_60m/_60m_4_path_test.csv', \
+#                 '_60m/_60m_5_path_test.csv', \
+#                 '_60m/_60m_6_path_test.csv', \
+#                 '_60m/_60m_7_path_test.csv', \
+#                 '_60m/_60m_8_path_test.csv', \
+#                 '_60m/_60m_9_path_test.csv', \
+#                 '_60m/_60m_10_path_test.csv']
 
-# csv_filename = ['_120m/_120m_0_path_test.csv', \
-#                 '_120m/_120m_1_path_test.csv', \
-#                 '_120m/_120m_2_path_test.csv', \
-#                 '_120m/_120m_3_path_test.csv', \
-#                 '_120m/_120m_4_path_test.csv', \
-#                 '_120m/_120m_5_path_test.csv', \
-#                 '_120m/_120m_6_path_test.csv', \
-#                 '_120m/_120m_7_path_test.csv', \
-#                 '_120m/_120m_8_path_test.csv', \
-#                 '_120m/_120m_9_path_test.csv', \
-#                 '_120m/_120m_10_path_test.csv']
+csv_filename = ['_120m/_120m_0_path_test.csv', \
+                '_120m/_120m_1_path_test.csv', \
+                '_120m/_120m_2_path_test.csv', \
+                '_120m/_120m_3_path_test.csv', \
+                '_120m/_120m_4_path_test.csv', \
+                '_120m/_120m_5_path_test.csv', \
+                '_120m/_120m_6_path_test.csv', \
+                '_120m/_120m_7_path_test.csv', \
+                '_120m/_120m_8_path_test.csv', \
+                '_120m/_120m_9_path_test.csv', \
+                '_120m/_120m_10_path_test.csv']
 
 ClientSocket = Method.SocketCreator()
 
@@ -89,54 +89,54 @@ def Send():
         for UAVindex in range(numUavs):
             temp = XYZ_NumPyArray[UAVindex]
             #For UAV to GND 
-            # if UAVindex == 1:
-            #     x = -220
-            #     y = 400
-            #     z = 0
-            # elif UAVindex == 3:
-            #     x = -220
-            #     y = 180
-            #     z = 0
-            # elif UAVindex == 5:
-            #     x = -620
-            #     y = 100
-            #     z = 0
-            # elif UAVindex == 7:
-            #     x = -600
-            #     y = -220
-            #     z = 0
-            # elif UAVindex == 9:
-            #     x = -200
-            #     y = -220
-            #     z = 0
-            # elif UAVindex == 2:
-            #     x = temp[Rowindex][0]
-            #     y = temp[Rowindex][1]
-            #     z = 0
-            # elif UAVindex == 4:
-            #     x = temp[Rowindex][0]
-            #     y = temp[Rowindex][1]
-            #     z = 0
-            # elif UAVindex == 6:
-            #     x = temp[Rowindex][0]
-            #     y = temp[Rowindex][1]
-            #     z = 0
-            # elif UAVindex == 8:
-            #     x = temp[Rowindex][0]
-            #     y = temp[Rowindex][1]
-            #     z = 0     
-            # elif UAVindex == 10:
-            #     x = temp[Rowindex][0]
-            #     y = temp[Rowindex][1]
-            #     z = 0         
-            # elif UAVindex == 0:
-            #     x = temp[Rowindex][0]
-            #     y = temp[Rowindex][1]
-            #     z = temp[Rowindex][2]
+            if UAVindex == 1:
+                x = -220
+                y = 400
+                z = 0
+            elif UAVindex == 3:
+                x = -220
+                y = 180
+                z = 0
+            elif UAVindex == 5:
+                x = -620
+                y = 100
+                z = 0
+            elif UAVindex == 7:
+                x = -600
+                y = -220
+                z = 0
+            elif UAVindex == 9:
+                x = -200
+                y = -220
+                z = 0
+            elif UAVindex == 2:
+                x = temp[Rowindex][0]
+                y = temp[Rowindex][1]
+                z = 0
+            elif UAVindex == 4:
+                x = temp[Rowindex][0]
+                y = temp[Rowindex][1]
+                z = 0
+            elif UAVindex == 6:
+                x = temp[Rowindex][0]
+                y = temp[Rowindex][1]
+                z = 0
+            elif UAVindex == 8:
+                x = temp[Rowindex][0]
+                y = temp[Rowindex][1]
+                z = 0     
+            elif UAVindex == 10:
+                x = temp[Rowindex][0]
+                y = temp[Rowindex][1]
+                z = 0         
+            elif UAVindex == 0:
+                x = temp[Rowindex][0]
+                y = temp[Rowindex][1]
+                z = temp[Rowindex][2]
             # UAV to UAV
-            x = temp[Rowindex][0]
-            y = temp[Rowindex][1]
-            z = temp[Rowindex][2]
+            # x = temp[Rowindex][0]
+            # y = temp[Rowindex][1]
+            # z = temp[Rowindex][2]
             toSend = "{X},{Y},{Z}".format(X = round(x, 1), Y = round(y, 1), Z = round(z, 1))
             print(toSend)
             print("UAV ", str(UAVindex))
