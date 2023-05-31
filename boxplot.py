6180086b0a1b0e0687a9529c18a042c049e2d3cf
Henok100 <henokgashaw15@gmail.com>
2023-05-31 17:18:43 +0300

    FInal

--- boxplot.py
+++ boxplot.py
@@ -3,29 +3,34 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 files = ['UAVtoUAVDense/FSP.csv', 'UAVtoUAVDense/TRG.csv', 'UAVtoUAVDense/LNS.csv', 'UAVtoUAVDense/RF.csv', 'UAVtoUAVDense/NF.csv']
+data = []
+xlabel = ['FSP', 'TRG', 'LNS', 'RF', 'NF']
 
 # Read the CSV file into a pandas DataFrame
 for i in range(5):
-    data = pd.read_csv(files[i])
+    data.append(pd.read_csv(files[i]))
 
+# Create a figure and axis object
+fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(15, 15))
 
-    # Extract the header row as a list
-    headers = list(data.columns)
+# Extract the header row as a list
+headers = list(data[0].columns)
 
-    # Create a figure and axis object
-    fig, ax = plt.subplots()
+# Create a box plot of the data
+for i, ax in enumerate(axs):
+    ax.boxplot(data[i])
+    ax.set_xticklabels(headers)
 
-    # Create a box plot of the data
-    ax.boxplot(data)
-
-    # Set the y-axis tick formatter to display values as percent
+# Set the y-axis tick formatter to display values as percent
+for i, ax in enumerate(axs):
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
-    plt.xticks(range(1, len(headers) + 1), headers)
+    ax.set_xlabel(xlabel[i])
+    ax.set_ylabel('PDR (%)')
 
-    # Add axis labels and a title to the plot
-    plt.xlabel('FreeSpacePropagation')
-    plt.ylabel('PDR(%)')
-    plt.title('Packet Delivery Ratio')
+# Add axis labels and a title to the plot
+# plt.xlabel('FreeSpacePropagation')
+# plt.ylabel('PDR(%)')
+# plt.title('Packet Delivery Ratio')
 
-    # Display the plot
-    plt.show()
+# Display the plot
+plt.show()
