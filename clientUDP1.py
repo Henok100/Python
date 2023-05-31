539353bd68e3999371e1ec0b47effebc44bb473d
Henok100 <henokgashaw15@gmail.com>
2023-05-31 16:08:40 +0300

    FInal

--- clientUDP1.py
+++ clientUDP1.py
@@ -89,54 +89,54 @@ def Send():
         for UAVindex in range(numUavs):
             temp = XYZ_NumPyArray[UAVindex]
             #For UAV to GND 
-            if UAVindex == 1:
-                x = -220
-                y = 400
-                z = 0
-            elif UAVindex == 3:
-                x = -220
-                y = 180
-                z = 0
-            elif UAVindex == 5:
-                x = -620
-                y = 100
-                z = 0
-            elif UAVindex == 7:
-                x = -600
-                y = -220
-                z = 0
-            elif UAVindex == 9:
-                x = -200
-                y = -220
-                z = 0
-            elif UAVindex == 2:
-                x = temp[Rowindex][0]
-                y = temp[Rowindex][1]
-                z = 0
-            elif UAVindex == 4:
-                x = temp[Rowindex][0]
-                y = temp[Rowindex][1]
-                z = 0
-            elif UAVindex == 6:
-                x = temp[Rowindex][0]
-                y = temp[Rowindex][1]
-                z = 0
-            elif UAVindex == 8:
-                x = temp[Rowindex][0]
-                y = temp[Rowindex][1]
-                z = 0     
-            elif UAVindex == 10:
-                x = temp[Rowindex][0]
-                y = temp[Rowindex][1]
-                z = 0         
-            elif UAVindex == 0:
-                x = temp[Rowindex][0]
-                y = temp[Rowindex][1]
-                z = temp[Rowindex][2]
+            # if UAVindex == 1:
+            #     x = -220
+            #     y = 400
+            #     z = 0
+            # elif UAVindex == 3:
+            #     x = -220
+            #     y = 180
+            #     z = 0
+            # elif UAVindex == 5:
+            #     x = -620
+            #     y = 100
+            #     z = 0
+            # elif UAVindex == 7:
+            #     x = -600
+            #     y = -220
+            #     z = 0
+            # elif UAVindex == 9:
+            #     x = -200
+            #     y = -220
+            #     z = 0
+            # elif UAVindex == 2:
+            #     x = temp[Rowindex][0]
+            #     y = temp[Rowindex][1]
+            #     z = 0
+            # elif UAVindex == 4:
+            #     x = temp[Rowindex][0]
+            #     y = temp[Rowindex][1]
+            #     z = 0
+            # elif UAVindex == 6:
+            #     x = temp[Rowindex][0]
+            #     y = temp[Rowindex][1]
+            #     z = 0
+            # elif UAVindex == 8:
+            #     x = temp[Rowindex][0]
+            #     y = temp[Rowindex][1]
+            #     z = 0     
+            # elif UAVindex == 10:
+            #     x = temp[Rowindex][0]
+            #     y = temp[Rowindex][1]
+            #     z = 0         
+            # elif UAVindex == 0:
+            #     x = temp[Rowindex][0]
+            #     y = temp[Rowindex][1]
+            #     z = temp[Rowindex][2]
             # UAV to UAV
-            # x = temp[Rowindex][0]
-            # y = temp[Rowindex][1]
-            # z = temp[Rowindex][2]
+            x = temp[Rowindex][0]
+            y = temp[Rowindex][1]
+            z = temp[Rowindex][2]
             toSend = "{X},{Y},{Z}".format(X = round(x, 1), Y = round(y, 1), Z = round(z, 1))
             print(toSend)
             print("UAV ", str(UAVindex))
