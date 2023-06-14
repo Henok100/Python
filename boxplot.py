edf4a8b2196ac0cd1fa99433fc14a6a06b3f9cc8
Henok100 <henokgashaw15@gmail.com>
2023-06-14 20:58:47 +0300

    Final

--- boxplot.py
+++ boxplot.py
@@ -2,16 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-files = ['UAVtoUAVDense/FSP.csv', 'UAVtoUAVDense/TRG.csv', 'UAVtoUAVDense/LNS.csv', 'UAVtoUAVDense/RF.csv', 'UAVtoUAVDense/NF.csv']
+#files = ['UAVtoUAVDense/FSP.csv', 'UAVtoUAVDense/TRG.csv', 'UAVtoUAVDense/LNS.csv', 'UAVtoUAVDense/RF.csv', 'UAVtoUAVDense/NF.csv']
+files = ['UAVtoUAVDense/LNS.csv', 'UAVtoUAVDense/RF.csv', 'UAVtoUAVDense/NF.csv']
+
 data = []
-xlabel = ['FreeSpacePathLoss', 'TwoRayGroundReflection', 'LogNormalShadowing', 'RicianFading', 'NakagamiFading']
+xlabel = ['LogNormalShadowing', 'RicianFading', 'NakagamiFading']
 
 # Read the CSV file into a pandas DataFrame
-for i in range(5):
+for i in range(3):
     data.append(pd.read_csv(files[i]))
 
 # Create a figure and axis object
-fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(15, 15))
+fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 15))
 
 # Extract the header row as a list
 headers = list(data[0].columns)
@@ -27,12 +29,13 @@ fig.subplots_adjust(wspace=0.4)
 
 # Set the y-axis tick formatter to display values as percent
 for i, ax in enumerate(axs):
+    ax.set_ylim(0, 1)
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
-    ax.set_xlabel(xlabel[i]).set_color('blue')
-    ax.set_ylabel('Packet Delivery Ratio ').set_color('blue')
+    ax.set_xlabel(xlabel[i], fontsize = 16).set_color('blue')
+    ax.set_ylabel('Packet Delivery Ratio', fontsize = 16).set_color('blue')
 
 #Super Title
-plt.suptitle("UAV-to-UAV Performance in Urban Environment", fontsize=16).set_color('blue')
+plt.suptitle("UAV-to-UAV Performance in Urban Environment", fontsize=24).set_color('blue')
 
 # Display the plot
 plt.show()
