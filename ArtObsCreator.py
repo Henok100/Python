ba4114fb54e8cda990cb859faba2d9e64c0ff76d
Henok100 <henokgashaw15@gmail.com>
2023-07-22 14:31:59 +0300

    Final

--- ArtObsCreator.py
+++ ArtObsCreator.py
@@ -1,18 +1,20 @@
 ###     Artificial map creator      ###
+###     Henok Gashaw, Bahir Dar, Ethiopia
+###     2023 GC
 
 import random
-file = open("DenseArtificalmap.xml", "w")
+file = open("BarcelonaStyle.xml", "w")
 
 file.write("<environment>\n")
 file.write('<material id="1" resistivity="100" relativePermittivity="4.5" relativePermeability="1" /> <!-- concrete-->\n')
 
-for i in range(-800, 820, 20):
+for i in range(-1000, 1000, 20):
     if i == 240 or i == 220 or i == 200 or i == -240 or i == -220 or i == -200 or i == -640 or i == -620 or i == -600 or i == 640 or i == 620 or i == 600 or i == 0:
         continue
-    for j in range(-800, 820, 20):
+    for j in range(-1000, 1000, 20):
         if j == 240 or j == 220 or j == 200 or j == -240 or j == -220 or j == -200 or j == -640 or j == -620 or j == -600 or j == 640 or j == 620 or j == 600 or i == 0:
             continue
-        x = f'<object position="min {i} {j} 0" orientation=" 0 0 0" shape="cuboid {random.randint(10, 15)} {random.randint(10, 15)} {random.randint(50, 150)} " material="1" />\n'
+        x = f'<object position="min {i} {j} 0" orientation=" 0 0 0" shape="cuboid {random.randint(10, 15)} {random.randint(10, 15)} {random.randint(25, 100)} " material="1" />\n'
         file.write(x)
 
 file.write('</environment>')
